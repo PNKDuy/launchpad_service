ce59793079b21aa9330a5d13a3dca3e106c8443d
Derrick Phung <phngkhuongduy@gmail.com>
2021-08-11 11:27:18 +0700

    updated database name

--- model/connector.go
+++ model/connector.go
@@ -6,7 +6,7 @@ import (
 )
 
 func ConnectToPostgres() (*gorm.DB, error) {
-	dsn := "host=127.0.0.1 user=postgres password=Adda@12354 dbname=launchpad_service port=5432 sslmode=disable"
+	dsn := "host=127.0.0.1 user=postgres password=Adda@12354 dbname=postgres port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
