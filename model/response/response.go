8fdce526b677ca14c9fb4f2821a06a68687824fb
Derrick Phung <phngkhuongduy@gmail.com>
2021-08-25 14:15:33 +0700

    update currency struct

--- model/response/response.go
+++ model/response/response.go
@@ -7,3 +7,11 @@ type Response struct {
 	PriceChangePercent string `json:"priceChangePercent"`
 	Volume string `json:"volume"`
 }
+
+type Currency struct {
+	Symbol string `json:"symbol"`
+	Price float64 `json:"price"`
+	PriceChange float64 `json:"priceChange"`
+	PriceChangePercent string `json:"priceChangePercent"`
+	Volume string `json:"volume"`
+}
